61ecc2e02c878500ac408296987da0d8c2c75d6c
Benny <gatts@live.se>
2021-10-26 10:10:06 +0200

    SendingMainAndRenderFile3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int s = 0;
+      
 	// write your code here
         Scanner input = new Scanner(System.in);
 
