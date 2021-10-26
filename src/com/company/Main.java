b1648580e7c543bc5552921220b68ed64e40e690
Benny <gatts@live.se>
2021-10-26 09:45:25 +0200

    SendingMainAndRenderFile

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int s = 0;
+	// write your code here
+        Scanner input = new Scanner(System.in);
+
+
+        Render sa = new Render();
+        System.out.println("sadjahdasda");
+        System.out.println("sadjahdasda");
+        System.out.println("sadjahdasda");
+        sa.renderFunctionThree();
+        sa.renderFunctionTwo(sa.menuList,"CHOOSE YOUR ACTION:");
+
+        System.out.println("input");
+
+        String choice = input.nextLine();
+
+
+
+
+
+
+    }
+
+
+
+
+}
