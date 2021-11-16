12f3f8c789a16b568fc25f9b593909860da119af
Ekiram <ekiram.salih@edu.newton.se>
2021-11-16 21:55:48 +0100

    Game Over

--- src/ActionSweep3.java
+++ src/ActionSweep3.java
@@ -4,37 +4,46 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ActionSweep3 {
-     boolean isTrue = false;
-     String message = "Game Over";
+    boolean isTrue = false;
+    String message = "Game Over";
 
-     public void gameOver(ArrayList<Objekt> objects){
-         Render pullFromRender = new Render();
+    public void gameOver(ArrayList<Objekt> objects) {
+        Render pullFromRender = new Render();
 
-         for(Objekt object:objects){
-             if(object.isBomb() ) {
-               object.setState("*");
-             }
-
-
-         }
-         Render render = new Render();
-      String message = " Game Over";
-
+        for (Objekt object : objects) {
+            if (object.isBomb()) {
+            } else {
+                object.isFlagg();
+                object.setState("*");
+                object.setState("F");
+            }
+        }
 
 
+        for (Objekt object : objects) {
+            if (object.isBomb()) {
+                object.setState("*");
+            }
 
 
+        }
+        for (Objekt object : objects) {
+            if (object.isFlagg()) {
+                object.setState("F");
+            }
+            {
+                break;
+            }
 
+        }
 
+        Render render = new Render();
+        String message = " Game Over";
 
+        {
 
-
-
-
-
+        }
 
 
     }
 }
-
-
