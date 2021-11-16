fe62740f54a4ef757b84739ea836f92b78cd2452
maishaafout <92914117+maishaafout@users.noreply.github.com>
2021-11-16 15:13:28 +0100

    Add files via upload

--- src/Action.java
+++ src/Action.java
@@ -6,46 +6,40 @@ public class Action {
 
 
 
-    static void myMethod(String actioninput, Objekt objekt) {
-        Scanner input = new Scanner(System.in);
+    public String myMethod(String actioninput, ArrayList<Objekt> objectList) {
+
 
 
         String message = "CHOOSE YOUR ACTION";
         if (actioninput.length() == 3) {
-            String[] letters = {"  ","A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
-            for (int i = 0; i < 9; i++) {
             String[] splitstring = actioninput.toUpperCase().split("");
 
-            if (splitstring[0].contains("S") ||splitstring[0].contains("F")){
+            if (splitstring[0].contains("S") || splitstring[0].contains("F")) {
+                for (int j = 0; j < 100; j++)
+                    if (objectList.get(j).getName().equals(splitstring[1] + splitstring[2])) {
+                        if (splitstring[0].contains("S")) {
+                            if (objectList.get(j).isFlagg()) {
+                                message = "CAN SWEEP because it is flagged";
+                            }
+                            if (!objectList.get(j).isFlagg()) {
 
-            }
-            if (splitstring[0].equals("S") ||splitstring[0].equals("F")){
-                System.out.println("WRONG ACTION KEY");
-
-                if(splitstring[i].contains("")){
-                    System.out.println(splitstring[i]);
-                    if(letters.equals(splitstring[0])){
-
-                        if (splitstring[i].contains("Not Flagged")){
-                            objekt.setFlagg(!objekt.isFlagg());
-                            System.out.println("CAN SWEEP because it is not flagged");
+                            }
                         }
-                        if(splitstring[i].contains("Flagged")){
-                            objekt.setFlagg(objekt.isFlagg());
-                            System.out.println("CAN'T SWEEP because it is flagged");
-                        }
-                        String Newmessage = "YOU LOST!!!!!";
+                    }
+
+            } else if (!splitstring[0].contains("S") || !splitstring[0].contains("F")) {
+
+                message = "WRONG ACTION KEY";
+
+
             }
 
-
-
-
-                   }
-
-
-               }
-            }
 
         }
+
+        return message;
     }
 }
+
+
+
