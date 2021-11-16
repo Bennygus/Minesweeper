36f64c86952997ebb84346bb7ba6c3e3880ab058
Ekiram Salih <ekiram.salih@Ekirams-MacBook-Pro.local>
2021-11-16 12:50:01 +0100

    sweep3

---
+++ src/ActionSweep3.java
@@ -0,0 +1,28 @@
+package com.company.src;
+
+import java.util.Scanner;
+
+public class ActionSweep3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("X");
+        String[] letter = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i= 0; i < 1; i++) {
+            for (int randomBomb = 0; randomBomb < 1; randomBomb++) {
+                System.out.print("*");
+                System.out.println("");
+                System.out.println("GAME OVER");
+
+
+            }
+
+
+            }
+
+
+        }
+
+
+    }
